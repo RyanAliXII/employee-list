3c5205993cdce73428418484748db82f567b1847
Ryan Ali <ryanali456@gmail.com>
2024-09-13 16:26:24 +0800

    configure to json response of server to be camel case

--- EmployeeServer/Program.cs
+++ EmployeeServer/Program.cs
@@ -1,8 +1,17 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+//make camel case the default key of json responses
+builder.Services.AddControllers(options=>{
+    options.ModelMetadataDetailsProviders.Add(new SystemTextJsonValidationMetadataProvider());
+}).AddJsonOptions(options=>{
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
